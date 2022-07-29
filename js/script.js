fea9a285a7f44f18093f2258486377b995cc0980
Jeremiah Ibiang Eni <77592477+Jerryeni@users.noreply.github.com>
2022-07-29 14:50:02 +0100

    initialize
    Add all

--- js/script.js
+++ js/script.js
@@ -33,9 +33,206 @@ yaxis: {
 }
 };
 
-var chart = new ApexCharts(document.querySelector("#chart1"), options);
+var chart = new ApexCharts(document.querySelector("#chart6"), options);
 chart.render();
 
+// Second chart
+
+var options = {
+  series: [{
+  name: 'Net Profit',
+  data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+}, {
+  name: 'Revenue',
+  data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+}, {
+  name: 'Free Cash Flow',
+  data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+}],
+  chart: {
+  type: 'bar',
+  height: 350
+},
+plotOptions: {
+  bar: {
+    horizontal: false,
+    columnWidth: '55%',
+    endingShape: 'rounded'
+  },
+},
+dataLabels: {
+  enabled: false
+},
+stroke: {
+  show: true,
+  width: 2,
+  colors: ['transparent']
+},
+xaxis: {
+  categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+},
+yaxis: {
+  title: {
+    text: '$ (thousands)'
+  }
+},
+fill: {
+  opacity: 1
+},
+tooltip: {
+  y: {
+    formatter: function (val) {
+      return "$ " + val + " thousands"
+    }
+  }
+}
+};
+
+var chart = new ApexCharts(document.querySelector("#chart2"), options);
+chart.render();
+
+
+
+
+// Third chart
+
+var options = {
+  series: [{
+  name: 'PRODUCT A',
+  data: [44, 55, 41, 67, 22, 43]
+}, {
+  name: 'PRODUCT B',
+  data: [13, 23, 20, 8, 13, 27]
+}, {
+  name: 'PRODUCT C',
+  data: [11, 17, 15, 15, 21, 14]
+}, {
+  name: 'PRODUCT D',
+  data: [21, 7, 25, 13, 22, 8]
+}],
+  chart: {
+  type: 'bar',
+  height: 350,
+  stacked: true,
+  toolbar: {
+    show: true
+  },
+  zoom: {
+    enabled: true
+  }
+},
+responsive: [{
+  breakpoint: 480,
+  options: {
+    legend: {
+      position: 'bottom',
+      offsetX: -10,
+      offsetY: 0
+    }
+  }
+}],
+plotOptions: {
+  bar: {
+    horizontal: false,
+    borderRadius: 10
+  },
+},
+xaxis: {
+  type: 'datetime',
+  categories: ['01/01/2011 GMT', '01/02/2011 GMT', '01/03/2011 GMT', '01/04/2011 GMT',
+    '01/05/2011 GMT', '01/06/2011 GMT'
+  ],
+},
+legend: {
+  position: 'right',
+  offsetY: 40
+},
+fill: {
+  opacity: 1
+}
+};
+
+var chart = new ApexCharts(document.querySelector("#chart3"), options);
+chart.render();
+
+
+
+
+
+// voter frequency chart
+
+// first voter frequency chart
+
+var options = {
+  series: [{
+  name: 'series1',
+  data: [31, 40, 28, 51, 42, 109, 100]
+}, {
+  name: 'series2',
+  data: [11, 32, 45, 32, 34, 52, 41]
+}],
+  chart: {
+  height: 350,
+  type: 'area'
+},
+dataLabels: {
+  enabled: false
+},
+stroke: {
+  curve: 'smooth'
+},
+xaxis: {
+  type: 'datetime',
+  categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+},
+tooltip: {
+  x: {
+    format: 'dd/MM/yy HH:mm'
+  },
+},
+};
+
+var chart = new ApexCharts(document.querySelector("#chart4"), options);
+chart.render();
+
+
+// second one
+
+var options = {
+  series: [{
+    name: "Desktops",
+    data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+}],
+  chart: {
+  height: 350,
+  type: 'line',
+  zoom: {
+    enabled: false
+  }
+},
+dataLabels: {
+  enabled: false
+},
+stroke: {
+  curve: 'straight'
+},
+title: {
+  text: 'Product Trends by Month',
+  align: 'left'
+},
+grid: {
+  row: {
+    colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+    opacity: 0.5
+  },
+},
+xaxis: {
+  categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+}
+};
+
+var chart = new ApexCharts(document.querySelector("#chart5"), options);
+chart.render();
 
 // Third chart
 
@@ -83,7 +280,125 @@ tooltip: {
 }
 };
 
-var chart = new ApexCharts(document.querySelector("#chart3"), options);
+var chart = new ApexCharts(document.querySelector("#chart1"), options);
+chart.render();
+
+// pie charts
+
+// first pie
+
+var options = {
+  series: [44, 55, 41, 17, 15],
+  chart: {
+  type: 'pie',
+},
+responsive: [{
+  breakpoint: 480,
+  options: {
+    chart: {
+      width: 200
+    },
+    legend: {
+      position: 'bottom'
+    }
+  }
+}]
+};
+
+var chart = new ApexCharts(document.querySelector("#chart7"), options);
 chart.render();
 
 
+// doughnut
+
+var options = {
+  series: [44, 55, 41, 17, 15],
+  chart: {
+  type: 'donut',
+},
+responsive: [{
+  breakpoint: 480,
+  options: {
+    chart: {
+      width: 200
+    },
+    legend: {
+      position: 'bottom'
+    }
+  }
+}]
+};
+
+var chart = new ApexCharts(document.querySelector("#chart8"), options);
+chart.render();
+
+
+// radial chart for faculty election
+
+var options = {
+  series: [44, 55, 67, 83],
+  chart: {
+  height: 350,
+  type: 'radialBar',
+},
+plotOptions: {
+  radialBar: {
+    dataLabels: {
+      name: {
+        fontSize: '22px',
+      },
+      value: {
+        fontSize: '16px',
+      },
+      total: {
+        show: true,
+        label: 'Total',
+        formatter: function (w) {
+          // By default this function returns the average of all series. The below is just an example to show the use of custom formatter function
+          return 249
+        }
+      }
+    }
+  }
+},
+labels: ['Apples', 'Oranges', 'Bananas', 'Berries'],
+};
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
+
+
+var options = {
+          series: [44, 55, 67, 83],
+          chart: {
+          height: 350,
+          type: 'radialBar',
+        },
+        plotOptions: {
+          radialBar: {
+            dataLabels: {
+              name: {
+                fontSize: '22px',
+              },
+              value: {
+                fontSize: '16px',
+              },
+              total: {
+                show: true,
+                label: 'Total',
+                formatter: function (w) {
+                  // By default this function returns the average of all series. The below is just an example to show the use of custom formatter function
+                  return 249
+                }
+              }
+            }
+          }
+        },
+        labels: ['Apples', 'Oranges', 'Bananas', 'Berries'],
+        };
+
+        var chart = new ApexCharts(document.querySelector("#chart9"), options);
+        chart.render();
+      
+      
+    
